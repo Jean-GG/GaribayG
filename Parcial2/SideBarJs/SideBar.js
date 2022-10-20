bd9eb4231a65f145fb6fa1602d5ccc8b574cf1ed
Jean-GG <jcgaribayg02@hotmail.com>
2022-10-20 14:27:28 -0700

    Boostrap

---
+++ Parcial2/SideBarJs/SideBar.js
@@ -0,0 +1,6 @@
+window.onload=function(){
+    document.getElementById("btnMenu"),addEventListener("click",function(){
+        console.log("Click");
+        document.getElementById("barralat").classList.toggle("escondido");
+    })
+}
