080c9e7a9b2cb0f4ba798676c6111c842b7d0cee
Jean-GG <jcgaribayg02@hotmail.com>
2022-11-18 10:44:54 -0800

    ConeccionBD

--- Parcial3/MySQL/connect.php
+++ Parcial3/MySQL/connect.php
@@ -10,7 +10,8 @@ $consulta = "select * from Datos";
 $registros = mysqli_query($con,$consulta) or die("Problemas con el select");
 
 while($reg = mysqli_fetch_array($registros)){
-    printf($reg['nombre'].''.$reg['apPaterno'].'<br>');
+    printf($reg['Nombre'].' '.$reg['Rfc'].' '.$reg['Telefono'].' '.$reg['Curp']. ' '
+    .$reg['Domicilio'].' '.$reg['CodigoPostal'].' '.$reg['Puesto'].' '.$reg['EstadoCivil'].'<br>');
 }
 mysqli_close($con);
-?>
+?>  
