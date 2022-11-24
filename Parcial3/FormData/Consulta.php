1b9bbc193f7e1055e9ada031d6c114418a39a995
Jean-GG <jcgaribayg02@hotmail.com>
2022-11-23 18:20:22 -0800

    UpdateFD

--- Parcial3/FormData/Consulta.php
+++ Parcial3/FormData/Consulta.php
@@ -1,16 +1,15 @@
 <?php
-$id = $_POST['Rfc']
+$id = $_POST['Rfc'];
 
 $servidor = "localhost";
-$basedatos = "EmpleadosMc";
+$basedatos = "empleadosmc";
 $usuario = "root";
 $password = "";
 
 $con = mysqli_connect($servidor,$usuario,$password,$basedatos) or die("No se pudo conectar a localhost");
-$request = "select * from datos where Rfc=$id";
-$register = mysqli_query($con,$request) or die("Problema en select");
-$result = mysqli_fetch_all($register);
+$consulta = "select * from datos where Rfc='$id'";
+$registros = mysqli_query($con,$consulta) or die("Problema en select");
+$result = mysqli_fetch_array($registros,MYSQLI_ASSOC);
 
-mysqli_close($con);
-echo json_encode($result);
+echo json_encode($result);   
 ?>  
